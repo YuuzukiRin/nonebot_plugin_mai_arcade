7eb29d0a8beb3c81f30676054a7bde05cb0c2cf2
YuuzukiRin <114254083+YuuzukiRin@users.noreply.github.com>
2024-10-21 00:59:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel
 from typing import Dict, List
 from nonebot import require
 require("nonebot_plugin_localstore")
 from nonebot_plugin_localstore import get_data_dir
-from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
-
-class Config(BaseModel, extra=Extra.ignore):
+class Config(BaseModel):
     mai_arcade_path: str = os.getenv("mai_arcade_path", None)
