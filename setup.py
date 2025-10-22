256bcbb04e5c06335c7632df27a913f4cc5899f2
YuuzukiRin <2824900523@qq.com>
2025-10-22 21:24:52 +0800

    refactor: update package structure and setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding="utf-8") as f:
 
 setup(
     name='nonebot-plugin-mai-arcade',
-    version='0.1.5.7.5',
+    version='0.2.0',
     description='maimai arcade nonebot2 plugin',
     long_description=long_description,
     long_description_content_type='text/markdown',
